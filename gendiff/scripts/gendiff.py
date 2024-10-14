f716391aa59812d862bbc696ea6dbe60907e9186
Igor Bessonov <ikogep23@gmail.com>
2024-10-14 22:03:18 +0300

    revert changes in gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 import argparse
-# from gendiff.diff import generate_diff
-from gendiff.file_reader import read_file, get_format
-from gendiff.parser import parse
-from gendiff.data_comparer import compare_data
-from gendiff.formatters.formatter import format_diff
-
-
-def generate_diff(filepath1, filepath2, formatter="stylish"):
-    format1 = get_format(filepath1)
-    format2 = get_format(filepath2)
-    data1 = parse(read_file(filepath1), format1)
-    data2 = parse(read_file(filepath2), format2)
-    diff = compare_data(data1, data2)
-    return format_diff(diff, formatter)
+from gendiff.diff import generate_diff
 
 
 def get_args():
     parser = argparse.ArgumentParser(
+        prog='gendiff',
         description='Compares two configuration files and shows a difference.',
         epilog='gendiff created by Bessonov Igor'
     )
