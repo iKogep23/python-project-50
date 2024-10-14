934342af33babb9f767dfddcd138ba0772745b1a
Igor Bessonov <ikogep23@gmail.com>
2024-10-14 21:52:47 +0300

    try to fix hexlet-check build error

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 import argparse
+# from gendiff.diff import generate_diff
+from gendiff.file_reader import read_file, get_format
+from gendiff.parser import parse
+from gendiff.data_comparer import compare_data
+from gendiff.formatters.formatter import format_diff
 
 
-from gendiff.diff import generate_diff
+def generate_diff(filepath1, filepath2, formatter="stylish"):
+    format1 = get_format(filepath1)
+    format2 = get_format(filepath2)
+    data1 = parse(read_file(filepath1), format1)
+    data2 = parse(read_file(filepath2), format2)
+    diff = compare_data(data1, data2)
+    return format_diff(diff, formatter)
 
 
 def get_args():
